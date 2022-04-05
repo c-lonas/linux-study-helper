ef9d6055cfa31695f1371100e41a79533635aa99
envelope <chaselonas@gmail.com>
2022-04-05 15:26:03 -0600

    animated footer component

---
+++ components/AppFooter.tsx
@@ -0,0 +1,50 @@
+import React, { FunctionComponent } from 'react';
+
+import { motion } from "framer-motion";
+import { normalize } from 'path';
+
+
+const footer = {
+    hidden: { opacity: 0, x: -250, fontStyle: "italic"},
+    visible: { 
+        opacity: 1,
+        fontStyle: "normal",
+        x: 0,
+        transition: {
+            delayChildren: 1,
+            staggerChildren: 1,
+            type: 'spring', 
+        }
+    }
+}
+
+const itemAnchor = {
+    hidden: { opacity: 0},
+    visible: { opacity: 1, transition: { delay: 2.5 }}
+}
+
+
+const AppFooter: React.FC = () => {
+    return (
+        <footer>
+            <motion.p
+                variants={footer}
+                initial="hidden"
+                animate="visible"
+            > Created by <motion.a
+                            variants={itemAnchor}
+                            initial="hidden"
+                            animate="visible"
+                            whileHover={{ color: "var(--color-legend)" }}
+                            href='https://chaselonas.dev' 
+                            target="_blank"
+                        > 
+                            chaselonas.dev 
+                        </motion.a> 
+            </motion.p>
+        </footer>
+    )
+    
+}
+
+export default AppFooter
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+
 import AppHeader from '../components/AppHeader'
+import AppFooter from '../components/AppFooter'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Let's Learn Linux!</title>
+        <title> Let's Learn Linux! </title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.png" />
       </Head>
@@ -16,12 +18,12 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="" target="_blank"> Next.js! </a>
         </h1>
 
         <div className='option-cntr'>
           <div className='option-card option-card-1'>
-            <p className='option-card-text'>Begin</p>
+            <p className='option-card-text'> Begin </p>
           </div>
           {/* <div className='option-card option-card-2'>
             <p className='option-card-text'>Quiz Mode</p>
@@ -31,6 +33,7 @@ const Home: NextPage = () => {
 
 
       </main>
+      <AppFooter />
     </div>
   )
 }
